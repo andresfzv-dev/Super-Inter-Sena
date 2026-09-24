145123718d19c275e03d2511a19969707539b8ca
andresfzv-dev <264909460+andresfzv-dev@users.noreply.github.com>
2026-09-24 12:02:54 -0500

    refactor: usar com.superinter como paquete base del backend

--- src/test/java/com/superinter/SuperInterApplicationTests.java
+++ src/test/java/com/superinter/SuperInterApplicationTests.java
@@ -1,13 +1,13 @@
-package com.superinter.gestion_productos;
+package com.superinter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestionProductosApplicationTests {
+class SuperInterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
