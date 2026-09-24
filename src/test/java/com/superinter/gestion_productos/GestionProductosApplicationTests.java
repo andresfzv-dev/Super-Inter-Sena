1d94000e8d4576dfac71edba7902c426cc088d71
andresfzv-dev <264909460+andresfzv-dev@users.noreply.github.com>
2026-09-24 11:57:40 -0500

    build: inicializar proyecto Spring Boot 4.1 con Maven Wrapper, Java 21 y Lombok

---
+++ src/test/java/com/superinter/gestion_productos/GestionProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superinter.gestion_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
